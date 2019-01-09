6671f4c6dbda5351aee67c0e49be769dc8489278
dkria <dkria@dkrias-MacBook-Pro.local>
2019-01-08 21:33:06 -0800

    making the friends.js, home.html and survey.html files

---
+++ app/data/friends.js
@@ -0,0 +1,162 @@
+[
+    {
+        "name": "John Bean",
+        "photo": "https://pbs.twimg.com/profile_images/681369932207013888/CHESpTzF_400x400.jpg",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Jacob Richardson",
+        "photo": "https://www.criswellandcriswell.com/assets/img/random/men-4.jpg",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Jeremy Washington",
+        "photo": "https://i.pinimg.com/736x/f8/4e/58/f84e58d21890a4d8fdbed123af94dba8.jpg",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Rodel Naval",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Rodel_Naval.jpg/1200px-Rodel_Naval.jpg",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Gerry Ritter",
+        "photo": "https://i.pinimg.com/236x/43/26/e0/4326e09f3e0106a642ab984535a222ca--hot-beards-haircut-men.jpg",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Mae Dela Pena",
+        "photo": "https://www.goodfreephotos.com/albums/people/beautiful-lady-face-portrait.jpg",
+        "scores": [
+            "5",
+            "2",
+            "3",
+            "3",
+            "3",
+            "1",
+            "3",
+            "5",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Kelly Smith",
+        "photo": "https://richardwiseman.files.wordpress.com/2009/08/face2.jpg",
+        "scores": [
+            "5",
+            "3",
+            "5",
+            "1",
+            "3",
+            "1",
+            "2",
+            "2",
+            "4",
+            "4"
+        ]
+    },
+    {
+        "name": "Christina Peralta",
+        "photo": "https://data.whicdn.com/images/261801522/superthumb.jpg",
+        "scores": [
+            "5",
+            "4",
+            "3",
+            "2",
+            "2",
+            "3",
+            "4",
+            "2",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Cheryl Winstone",
+        "photo": "https://i.pinimg.com/originals/f5/87/34/f587347e06d6c3c9c0a3fc9675e6bdda.jpg",
+        "scores": [
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2"
+        ]
+    },
+    {
+        "name": "Myla Cheng",
+        "photo": "downlhttps://assets.rebelcircus.com/blog/wp-content/uploads/2016/11/Unknown9.jpgoad-1.jpg",
+        "scores": [
+            "5",
+            "3",
+            "3",
+            "1",
+            "5",
+            "2",
+            "5",
+            "4",
+            "3",
+            "4"
+        ]
+    }
+]
